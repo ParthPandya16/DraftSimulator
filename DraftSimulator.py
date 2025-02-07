8a920c47995385686387e86c52f9adc13f5b7be1
Parth Pandya <pparth16@vt.edu>
2025-02-07 16:56:51 -0500

    updated

--- DraftSimulator.py
+++ DraftSimulator.py
@@ -1,23 +1,26 @@
 from tkinter import *
+import numpy as np
 
 window = Tk()
-window.geometry("840x940")
+window.geometry("1700x900")
 window.title("ScoutGPT")
-icon = PhotoImage(file = 'Small_logo.png')
-window.iconphoto(True, icon)
 window.config(background = "black")
-title_label = Label(window, text = "ScoutGPT Draft Simulator", font = ('Arial', 40, 'bold'), fg = 'white', bg = 'green')
-title_label.pack()
-#draftBackground = PhotoImage(file = "Draft_background.png")
-#window.iconphoto(True, draftBackground)
+small_logo = PhotoImage(file = 'Small_logo.png')
+window.iconphoto(True, small_logo)
 
+background = PhotoImage(file = "Background.png")
+background_label = Label(window, image = background)
+background_label.place(x = 25, y = 0)
 
 teams = ["Titans", "Browns", "Giants", "Patriots", "Jaguars", "Raiders", "Jets", "Panthers",
          "Saints", "Bears", "49ers", "Cowboys", "Dolphins", "Colts", "Falcons", "Cardinals",
-         "Bengals", "Seahawks", "Buccaneers", "Broncos", "Steelers", "Chargers", "Packers", "Vikings"
+         "Bengals", "Seahawks", "Buccaneers", "Broncos", "Steelers", "Chargers", "Packers", "Vikings",
          "Texans", "Rams", "Ravens", "Lions", "Commanders", "Bills", "Eagles", "Chiefs"]
 
+current_pick = 1
 for current_team in teams:
-    print(current_team)
+    #draft()
+    print(str(current_pick) + ": " + current_team)
+    current_pick += 1
 
-#window.mainloop()
+window.mainloop()
